79fc6d290f7a78da6fc9777dd826111943c1de36
unknown <Eze Anthony>
2023-02-17 20:10:20 +0000

    first commit

---
+++ controllers/room.js
@@ -0,0 +1,84 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new Room(req.body);
+
+  try {
+    const savedRoom = await newRoom.save();
+
+    try {
+      //update the hotelid param by adding created & save room id into it
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id }, //use mongoDB push keyword to push the savedroom id into the hotelid param
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body }, //use set keyword to update rom id
+      { new: true }
+    );
+    res.status(200).json(updatedRoom); //send to user
+  } catch (err) {
+    next(err);
+  }
+};
+export const updateRoomAvailability = async (req, res, next) => {
+  try {
+    await Room.updateOne(
+      { "roomNumbers._id": req.params.id },
+      {
+        $push: {
+          "roomNumbers.$.unavailableDates": req.body.dates,
+        },
+      }
+    );
+    res.status(200).json("Room status has been updated.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id }, //pull is a mongoDB keyword use to remove/delete the room id param
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json("Room has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
