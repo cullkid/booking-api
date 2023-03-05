c34a459cac301b3b967fedfce7872f4a5500c445
unknown <Eze Anthony>
2023-03-05 17:42:28 +0000

    changed

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ import cors from "cors";
 const app = express();
 dotenv.config();
 
-// const connect = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO);
-//     console.log("Connected to mongoDB.");
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
 
-// // mongoose.connection.on("disconnected", () => {
-// //   console.log("mongoDB disconnected!");
-// // });
+// mongoose.connection.on("disconnected", () => {
+//   console.log("mongoDB disconnected!");
+// });
 
 // //middlewares
 app.use(cors());
@@ -34,36 +34,36 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-// app.use((err, req, res, next) => {
-//   const errorStatus = err.status || 500;
-//   const errorMessage = err.message || "Something went wrong!";
-//   return res.status(errorStatus).json({
-//     success: false,
-//     status: errorStatus,
-//     message: errorMessage,
-//     stack: err.stack,
-//   });
-// });
-
-// app.listen(8000, () => {
-//   connect();
-//   console.log("Connected to backend.");
-// });
-
-const PORT = process.env.PORT || 3000;
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("listening for requests");
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
   });
 });
+
+app.listen(8000, () => {
+  connect();
+  console.log("Connected to backend.");
+});
+
+// const PORT = process.env.PORT || 3000;
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log("listening for requests");
+//   });
+// });
